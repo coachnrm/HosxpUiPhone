a5a540d4ef93274aa170a04a2ea4ebea7f76f4bc
coachnrm <nuttapong_coach@hotmail.com>
2024-12-16 15:26:21 +0700

    first commit

---
+++ HosxpUiPhone/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HosxpUiPhone
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HosxpUiPhone/Customer.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HosxpUiPhone
+{
+    public class Customer
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+    }
+}
